5f29b6e45e531807d6c01f4ff8b7bf6f40408a86
Chizzo Cheese <chizzo84@gmail.com>
2018-12-02 21:43:10 +0200

    Adding basic authentication
    Adding basic user authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,14 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+			$table->string('mobile')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+			$table->engine = 'InnoDB';
+			$table->charset = 'utf8mb4';
+			$table->collation = 'utf8mb4_unicode_ci';
         });
     }
 
