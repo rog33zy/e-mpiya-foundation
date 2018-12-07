221edee111feefe0766b9b03a7394d6ae77dbf63
Chizzo Cheese <chizzo84@gmail.com>
2018-12-07 22:07:50 +0200

    Title change
    Change of title in head

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel {{ app()->version() }}</title>
+        <title>e-Mpiya OMPS</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
