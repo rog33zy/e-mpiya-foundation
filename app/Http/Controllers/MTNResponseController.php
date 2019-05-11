348cb1611ffdfeefd899bb52bc25c0c5616e2bb2
Chizzo Cheese <chizzo84@gmail.com>
2019-05-11 17:46:45 +0200

    JSON and SOAP URL Parameters
    Added JSON and SOAP URL Parameters to filter request and response

--- app/Http/Controllers/MTNResponseController.php
+++ app/Http/Controllers/MTNResponseController.php
@@ -137,7 +137,7 @@ XML;
 			</return>
 			<return>
 				<name>ThirdPartyAcctRef</name>
-				<value>121212</value>
+				<value>121212-soap</value>
 			</return>
 			<return>
 				<name>MOMTransactionID</name>
@@ -151,20 +151,130 @@ XML;
 		return $RESPONSE_BODY;
 		} elseif ($format == "json")
 		{
-		$ERROR_RESPONSE= <<<XML
+		// get incoming SOAP request contents
+		try {
+			$full_request = file_get_contents("php://input");
+		} catch (\Exception $e) {
+			$ERROR_RESPONSE= <<<XML
 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:b2b="http://b2b.mobilemoney.mtn.zm_v1.0/">
 	<soapenv:Header/>
 	<soapenv:Body>
 		<b2b:processRequestResponse>
 			<return>
 				<name>Error</name>
-				<value>No JSON data found</value>
+				<value>No data found</value>
 			</return>
 		</b2b:processRequestResponse>
 	</soapenv:Body>
 </soapenv:Envelope>
 XML;
 			return $ERROR_RESPONSE;
+		}
+		// get SOAP request XML
+		$request_xml = new \SimpleXMLElement(strstr($full_request, '<'), LIBXML_NOERROR);
+		
+		// get an array of SOAP request parameters
+		foreach ($request_xml->xpath('//parameter') as $parameter) {
+			switch($parameter->name) {
+				case "ProcessingNumber":
+					$ProcessingNumber = $parameter->value;
+					break;
+				case "serviceId":
+					$serviceId = $parameter->value;
+					break;
+				case "SenderID":
+					$SenderID = $parameter->value;
+					break;
+				case "PrefLang":
+					$PrefLang = $parameter->value;
+					break;
+				case "OpCoID":
+					$OpCoID = $parameter->value;
+					break;
+				case "MSISDNNum":
+					$MSISDNNum = $parameter->value;
+					break;
+				case "Amount":
+					$Amount = $parameter->value;
+					break;
+				case "Narration":
+					$Narration = $parameter->value;
+					break;
+				case "IMSINum":
+					$IMSINum = $parameter->value;
+					break;
+				case "OrderDateTime":
+					$OrderDateTime = $parameter->value;
+					break;
+			}
+		}
+		
+		// logged in user
+		$user = auth()->user();
+		
+		// client properties
+		// Header Details
+		// e-Mpiya client App generated
+		// status code
+		$StatusCode = 01;
+		
+		// status description code
+		$StatusDesc = "Successfully Processed Transaction.";
+		
+		// token
+		$Token = 121212;
+		
+		// generate response
+		$RESPONSE_BODY= <<<XML
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+	<soapenv:Body>
+		<ns1:processRequestResponse xmlns:ns1="http://b2b.mobilemoney.mtn.zm_v1.0/">
+			<return>
+				<name>ProcessingNumber</name>
+				<value>$ProcessingNumber</value>
+			</return>
+			<return>
+				<name>SenderID</name>
+				<value>$SenderID</value>
+			</return>
+			<return>
+				<name>StatusCode</name>
+				<value>$StatusCode</value>
+			</return>
+			<return>
+				<name>StatusDesc</name>
+				<value>$StatusDesc</value>
+			</return>
+			<return>
+				<name>OpCoID</name>
+				<value>$OpCoID</value>
+			</return>
+			<return>
+				<name>IMSINum</name>
+				<value>$IMSINum</value>
+			</return>
+			<return>
+				<name>MSISDNNum</name>
+				<value>$MSISDNNum</value>
+			</return>
+			<return>
+				<name>OrderDateTime</name>
+				<value>$OrderDateTime</value>
+			</return>
+			<return>
+				<name>ThirdPartyAcctRef</name>
+				<value>121212-json</value>
+			</return>
+			<return>
+				<name>MOMTransactionID</name>
+				<value>456</value>
+			</return>
+		</ns1:processRequestResponse>
+	</soapenv:Body>
+</soapenv:Envelope>
+XML;
+	
+		return $RESPONSE_BODY;
 		} else
 		{
 		$ERROR_RESPONSE= <<<XML
